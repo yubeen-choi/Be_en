711758bc327130b1f4a5cfb0162928e48ede45a6
yubeenCHOI <asdz001@naver.com>
2021-01-21 17:20:18 +0900

    BASIC Form_sign in

--- portfolio/Basic_form/js/main.js
+++ portfolio/Basic_form/js/main.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
    
-    var options = {
+    let options = {
         videoId : 'MzYYUGnmqLA'
         
     };		
 
     $('#wrapper').tubular(options);
+
+    let id = $('#id');
+    let pw = $('#pw');
+    let btn = $('#btn');
+
+    $(btn).on('click', function() {
+        if($(id).val() == ""){
+            $(id).next('label').addClass('warning');
+            setTimeout(function(){
+                $('label').removeClass('warning');
+            },1000);
+
+        }else if($(pw).val() == ""){
+            $(pw).next('label').addClass('warning');
+            setTimeout(function(){
+                $('label').removeClass('warning');
+            },1000);
+        }
+    });
 
 
 }); //$(function)
