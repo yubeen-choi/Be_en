2636b6aa7c09ecee0dead8840eee63e7702508a6
yubeenCHOI <asdz001@naver.com>
2021-01-21 21:08:51 +0900

    Basic_form_moblie_video_

--- portfolio/Basic_form/js/main.js
+++ portfolio/Basic_form/js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
-    
+
+    function webTubular() {
+    let options = {
+        videoId : 'MzYYUGnmqLA'
+        
+    };		
+
+    $('#wrapper').tubular(options);
+    } //webTubular
+
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
     jQuery(document).ready(function($){
          if(!isMobile) {
-            let options = {
-                videoId : 'MzYYUGnmqLA'
-                
-            };		
-        
-            $('#wrapper').tubular(options);
+            webTubular();
               //PC
          } else {
-            $('#wrapper').tubular(options).destroy();  //MOBILE
+            webTubular.destroy();  //MOBILE
          }
-    });
+    }); //webTubular_media
     
 
     // warning event
