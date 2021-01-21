dc602ba2a93c319c4cdd867921b0a427d11e5a83
yubeenCHOI <asdz001@naver.com>
2021-01-21 20:45:00 +0900

    BASIC_form_popup_modified

--- portfolio/Basic_form/js/main.js
+++ portfolio/Basic_form/js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
     $('#wrapper').tubular(options);
 
+    // warning event
     let id = $('#id');
     let pw = $('#pw');
     let btn = $('#btn');
@@ -24,8 +25,48 @@ $(document).ready(function() {
                 $('label').removeClass('warning');
             },1000);
         }
+    }); // warning event
+
+
+    // POPUP warning event
+    let popupid = $('#popupid');
+    let popupemail = $('#popupemail');
+    let popupbtn = $('#popupbtn');
+
+    $(popupbtn).on('click', function() {
+        if($(popupid).val() == ""){
+            $(popupid).next('label').addClass('warning');
+            setTimeout(function(){
+                $('label').removeClass('warning');
+            },1000);
+
+        }else if($(popupemail).val() == ""){
+            $(popupemail).next('label').addClass('warning');
+            setTimeout(function(){
+                $('label').removeClass('warning');
+            },1000);
+        }
+    }); // POPUP warning event
+
+
+    //POPUP on
+    let signUp = $('.signup')
+    let popup = $('.popup')
+
+    $(signUp).find('a').click(function(e){
+        e.preventDefault();
+        $(popup).addClass('on');
     });
 
+    
 
+    popup.click(function(){
+        $(popup).removeClass('on');
+    });
+    // POPUP on
+    
+
+
+    
 }); //$(function)
 
